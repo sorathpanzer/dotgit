ddf6e0b5c260133c8c3ca8361a9e519db5ed38d4
sorathpanzer <sorathpanzer@gmail.com>
2020-06-22 16:13:33 +0000

    tweaks

--- dotfiles/.config/suckless/autostart.sh
+++ dotfiles/.config/suckless/autostart.sh
@@ -4,7 +4,7 @@ picom &
 udiskie &
 numlockx &
 exec /usr/lib/kdeconnectd &
-hsetroot -full $HOME/Imagens/Wallpapers/literature-black.png &
+hsetroot -full $HOME/Imagens/Wallpapers/vader.png &
 xrandr --output "eDP-1" --brightness 0.70 &
 xinput --set-prop "SYNA2B46:00 06CB:CD5F Touchpad" "libinput Natural Scrolling Enabled" 1 &
 pactl set-sink-volume alsa_output.pci-0000_00_1f.3.analog-stereo 150% &
