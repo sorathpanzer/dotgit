50c276f79e5eb8b80a7b010a398f2d548b7dd5d1
sorathpanzer <sorathpanzer@gmail.com>
2020-07-19 20:01:03 +0000

    new strategy

---
+++ dotfiles/Projectos/StockS&P/main.R
@@ -0,0 +1,19 @@
+library(quantmod)
+library(ggplot2)
+library(magrittr)
+
+start_date <- as.Date("2008-01-03")
+dataEnv<-new.env()
+
+STOCK <- XRX
+SYMBOL <- c("XRX")
+length(SYMBOL)
+
+getSymbols(SYMBOL, from = start_date)
+#getSymbols(SYMBOL, src = "yahoo", from = start_date)
+getSymbols("SPY", from = start_date)
+
+CPR <- STOCK/SPY
+
+candleChart(STOCK, up.col = "green", dn.col = "red", theme = "black")
+candleChart(CPR, up.col = "green", dn.col = "red", theme = "black")
