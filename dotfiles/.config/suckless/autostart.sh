1909a4d453038ae0e12c47d28bafd9407b7c2963
sorathpanzer <sorathpanzer@gmail.com>
2020-08-28 23:20:09 +0100

    xresources

--- dotfiles/.config/suckless/autostart.sh
+++ dotfiles/.config/suckless/autostart.sh
@@ -5,7 +5,7 @@ udiskie &
 numlockx &
 #hsetroot -full $HOME/Imagens/Wallpapers/vader.png &
 hsetroot -solid black
-xrdb -load .Xresources &
+xrdb -load $HOME/.Xresources &
 xrandr --output "eDP-1" --brightness 0.70 &
 xinput --set-prop "SYNA2B46:00 06CB:CD5F Touchpad" "libinput Natural Scrolling Enabled" 1 &
 pactl set-sink-volume alsa_output.pci-0000_00_1f.3.analog-stereo 150% &
