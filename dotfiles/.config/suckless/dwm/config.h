8fe1fea60e1b222d20987a45e175d34729db1499
sorathpanzer <sorathpanzer@gmail.com>
2020-12-09 04:48:50 +0000

    fix clreboot

--- dotfiles/.config/suckless/dwm/config.h
+++ dotfiles/.config/suckless/dwm/config.h
@@ -80,7 +80,7 @@ static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() 
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "alacritty", NULL };
 static const char *shutcmd[]  = { "clpoweroff", NULL };
-static const char *rebcmd[]  = { "clpoweroff", NULL };
+static const char *rebcmd[]  = { "clreboot", NULL };
 
 static Key keys[] = {
 	/* modifier               key              function        argument */
@@ -113,7 +113,7 @@ static Key keys[] = {
 	{ MODKEY,                 XK_i,      	   	incnmaster,     {.i = +1 } },
   { MODKEY,           			XK_d,      	   	incnmaster,     {.i = -1 } },
 	{ MODKEY,                 XK_z, 	   			zoom,           {0} },
-	{ MODKEY,                 XK_u,      setlayout,      {.v = &layouts[2]} },
+	{ MODKEY,                 XK_u,      			setlayout,      {.v = &layouts[2]} },
 
     /* Gaps keybinds control */
 	{ MODKEY|ControlMask,     XK_g,      	   togglegaps,     {0} },
