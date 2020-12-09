4b168752dfb6b4a1bb44c5f779146c7912f9f059
sorathpanzer <sorathpanzer@gmail.com>
2020-12-09 04:49:42 +0000

    Start wireguard

--- dotfiles/.config/suckless/autostart.sh
+++ dotfiles/.config/suckless/autostart.sh
@@ -7,6 +7,7 @@ xrandr --output "eDP-1" --brightness 0.70 &
 xinput --set-prop "SYNA2B46:00 06CB:CD5F Touchpad" "libinput Natural Scrolling Enabled" 1 &
 pactl set-sink-volume alsa_output.pci-0000_00_1f.3.analog-stereo 150% &
 /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
+sudo wg-quick up wg0 &
 
 clock() {
   clock=$(date '+%I')
