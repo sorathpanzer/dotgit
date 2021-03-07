3ca9aa403cd26ded211f9afd1b9a29e7ada1e702
sorathpanzer <sorathpanzer@gmail.com>
2021-03-07 13:33:14 +0000

    Small fixes

--- dotfiles/.config/suckless/dwm/config.h
+++ dotfiles/.config/suckless/dwm/config.h
@@ -36,6 +36,7 @@ static const Rule rules[] = {
 	 */
 	/* class      instance    title       tags mask     switchtotag    isfloating   monitor */
 	{ "Chromium",    	NULL,       NULL,       1,            1,             0,           -1 },
+	{ "Brave",    		NULL,       NULL,       1,            1,             0,           -1 },
 	{ "qutebrowser",  NULL,       NULL,       1 << 3,       1,             0,           -1 },
 	{ "st-256color",  NULL,       NULL,       2,            1,             0,           -1 },
 	{ "Alacritty",    NULL,       NULL,       2,            1,             0,           -1 },
@@ -44,7 +45,7 @@ static const Rule rules[] = {
 	{ "Pcmanfm",     	NULL,       NULL,       1 << 2,       1,             0,           -1 },
 	{ "Telegram",    	NULL,       NULL,       1 << 2,       1,             0,           -1 },
 	{ "Signal",    		NULL,       NULL,       1 << 2,       1,             0,           -1 },
-	{ "Element",    		NULL,       NULL,       1 << 2,       1,             0,           -1 },
+	{ "Element",    	NULL,       NULL,       1 << 2,       1,             0,           -1 },
 	{ "Sxiv",     	 	NULL,       NULL,       1 << 2,       1,             0,           -1 },
 	{ "imv",         	NULL,       NULL,       1 << 2,       1,             0,           -1 },
 	{ "Steam",     	 	NULL,       NULL,       1 << 3,       1,             0,           -1 },
@@ -83,6 +84,7 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "alacritty", NULL };
+static const char *browsercmd[]  = { "brave", NULL };
 static const char *shutcmd[]  = { "clpoweroff", NULL };
 static const char *rebcmd[]  = { "clreboot", NULL };
 
