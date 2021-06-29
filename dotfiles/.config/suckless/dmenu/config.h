28a167e28cb4f58706c798347f8dcc3ac0a00a5d
sorathpanzer <sorathpanzer@gmail.com>
2021-06-29 14:00:33 +0100

    small up

--- dotfiles/.config/suckless/dmenu/config.h
+++ dotfiles/.config/suckless/dmenu/config.h
@@ -1,9 +1,15 @@
+/*  ____ _____  */
+/* |  _ \_   _|  Derek Taylor (DistroTube) */
+/* | | | || |  	http://www.youtube.com/c/DistroTube */
+/* | |_| || |  	http://www.gitlab.com/dwt1/ */
+/* |____/ |_|  	*/
+
 /* See LICENSE file for copyright and license details. */
 /* Default settings; can be overriden by command line. */
 
-static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
-static int centered = 0;                    /* -c option; centers dmenu on screen */
-static int min_width = 300;                    /* minimum width when centered */
+static int topbar = 1;       /* -b  option; if 0, dmenu appears at bottom     */
+static int centered = 0;     /* -c option; centers dmenu on screen */
+static int min_width = 300;  /* minimum width when centered */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
 	"Mononoki Nerd Font:pixelsize=16"
@@ -11,13 +17,15 @@ static const char *fonts[] = {
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#bbbbbb", "#222222" },
-	[SchemeSel] = { "#eeeeee", "#005577" },
+	[SchemeNorm] = { "#cccccc", "#282a36" },
+	[SchemeSel] = { "#ffffff", "#74438f" },
+	[SchemeSelHighlight] = { "#d7d7d7", "#000000" },
+	[SchemeNormHighlight] = { "#a5bdf8", "#000000" },
 	[SchemeOut] = { "#000000", "#00ffff" },
+	[SchemeMid] = { "#d7d7d7", "#1f2026" },
 };
-/* -l option; if nonzero, dmenu uses vertical list with given number of lines */
-static unsigned int lines      = 10;
-
+/* -l and -g options; controls number of lines and columns in grid if > 0 */
+static unsigned int lines      = 0;
 /*
  * Characters not considered part of a word while deleting words
  * for example: " /?\"&[]"
@@ -25,4 +33,4 @@ static unsigned int lines      = 10;
 static const char worddelimiters[] = " ";
 
 /* Size of the window border */
-static const unsigned int border_width = 1;
+static unsigned int border_width = 1;  /* -bw option; to add border width */
