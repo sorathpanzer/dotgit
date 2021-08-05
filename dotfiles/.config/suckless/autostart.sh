83ea2b60ab2780c41a4e7e6697ff9722c30dd2d4
sorathpanzer <sorathpanzer@gmail.com>
2021-08-05 04:51:07 +0100

    support for pulseaudio in void

--- dotfiles/.config/suckless/autostart.sh
+++ dotfiles/.config/suckless/autostart.sh
@@ -7,8 +7,13 @@ xrdb -load $HOME/.Xresources &
 xrandr --output "eDP-1" --brightness 0.70 &
 xinput --set-prop "SYNA2B46:00 06CB:CD5F Touchpad" "libinput Natural Scrolling Enabled" 1 &
 pactl set-sink-volume alsa_output.pci-0000_00_1f.3.analog-stereo 100% &
-/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
+#/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
 picom --config ~/.config/picom/picom.conf -b &
 syncthing -no-browser &
-hsetroot -cover $HOME/Imagens/Wallpapers/clouds.png
-#hsetroot -solid "#000000"
+hsetroot -cover $HOME/Imagens/Wallpapers/clouds.png &
+#hsetroot -solid "#000000" &
+
+if uname -n | grep -q 'void-live'; then
+  pulseaudio --daemonize=no --exit-idle-time=-1
+fi
+
