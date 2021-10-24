3291223f9a41d1cb962efd61d341ddaf3aa69303
sorathpanzer <sorathpanzer@gmail.com>
2021-10-24 01:46:08 +0100

    organize config file

--- dotfiles/.config/suckless/dwm/config.h
+++ dotfiles/.config/suckless/dwm/config.h
@@ -1,29 +1,34 @@
 /* See LICENSE file for copyright and license details. */
 
+static const char *fonts[]          = { "UbuntuMono Nerd Font:size=10" };
+static const char dmenufont[]       = { "UbuntuMono Nerd Font:size=10" };
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
-static const unsigned int snap      = 32;       /* snap pixel */
+static const unsigned int snap      = 10;       /* snap pixel */
+
+/* ----------------- gaps ---------------- */
 static const unsigned int gappih    = 8;       /* horiz inner gap between windows */
 static const unsigned int gappiv    = 8;       /* vert inner gap between windows */
 static const unsigned int gappoh    = 7;       /* horiz outer gap between windows and screen edge */
 static const unsigned int gappov    = 7;       /* vert outer gap between windows and screen edge */
 static const int smartgaps          = 1;        /* 1 means no outer gap when there is only one window */
+
+/* ----------------- bar ----------------- */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const Bool viewontag         = True;     /* Switch view on tag switch */
-static const int horizpadbar        = 6;        /* horizontal padding for statusbar */
+static const int horizpadbar        = 2;        /* horizontal padding for statusbar */
 static const int vertpadbar         = 7;        /* vertical padding for statusbar */
-static const char *fonts[]          = { "UbuntuMono Nerd Font:size=10" };
-static const char dmenufont[]       = "UbuntuMono Nerd Font:size=10";
+
+/* ------------- colorscheme ------------- */
+static const char col_black[]       = "#000000"; /* border color unfocused windows */
 static const char col_gray1[]       = "#292d3e"; /* #292d3e */
-static const char col_gray2[]       = "#000000"; /* border color unfocused windows */
 static const char col_gray3[]       = "#cccccc"; /* #96b5b4 */
 static const char col_gray4[]       = "#c0c5ce";
-static const char col_cyan[]        = "#74438e"; /* border color focused windows and tags DT=#645377 */
-static const char col_cyan2[]       = "#a5bdf7";
+static const char col_purple[]        = "#74438e"; /* border color focused windows and tags DT=#645377 */
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray4, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	[SchemeNorm] = { col_gray4, col_gray1, col_black },
+	[SchemeSel]  = { col_gray4, col_purple,  col_purple  },
 };
 
 typedef struct {
@@ -38,10 +43,27 @@ static Sp scratchpads[] = {
 	{"keepassxc",   spcmd2},
 };
 
-/* tagging */
-static const char *tags[] = { "", "", "", "" };
-static const char *alttags[] = { "|}|", "| |", "| |", "| |" };
+/* ----------------- tags ---------------- */
+static const char *tags[]       = { " ", " ", " ", " ", " ", " ", " " };
+static const char *alttags[]       = { "► ", "► ", "► ", "► ", "► ", "► ", "► " };
 
+/*static const char *tags[] = { "", "", "", "" };
+static const char *alttags[] = { "|}|", "| |", "| |", "| |" };*/
+
+/* -------------- autostart -------------- */
+static const char autostartsh[]     = "autostart.sh";
+static const char autostartwaitsh[] = "autostartwait.sh";
+static const char dwmdir[]          = "dwm";     /* parent dir of autostart(wait).sh */
+static const char pathfromhome[]    = ".config"; /* custom dir from $HOME if $XDG_CONFIG_HOME was not set; don't add leading & trailing '/' */
+
+/* --------------- defaults -------------- */
+#define APP_BROWSER             "firefox"
+#define APP_BROWSER_            "firefox --private-window"
+#define APP_MENU                "dmenurun"
+#define APP_TERMINAL            "st"
+#define CLASS_TERMINAL          "St"
+
+/* ---------------- rules ---------------- */
 static const Rule rules[] = {
 	/* xprop(1):
 	 *	WM_CLASS(STRING) = instance, class
@@ -51,7 +73,6 @@ static const Rule rules[] = {
 	{ "qutebrowser",    	NULL,       NULL,       1,            1,             0,           -1 },
 /*{ "Chromium",         NULL,       NULL,       1,            1,             0,           -1 },*/
 	{ "st-256color",      NULL,       NULL,       2,            1,             0,           -1 },
-/*	{ "Alacritty",        NULL,       NULL,       0,            0,             1,           -1 },*/
 	{ "mpv",     			    NULL,       NULL,       1 << 2,       1,             0,           -1 },
 	{ "Signal",    		    NULL,       NULL,       1 << 2,       1,             0,           -1 },
 	{ "Sxiv",     	 	    NULL,       NULL,       1 << 2,       1,             0,           -1 },
@@ -61,7 +82,7 @@ static const Rule rules[] = {
 	{ NULL,		            "keepassxc",NULL,		    SPTAG(1),		  0,             1,			      -1 },
 };
 
-/* layout(s) */
+/* --------------- layouts --------------- */
 static const float mfact     = 0.55; /* factor of master area size [0.05..0.95] */
 static const int nmaster     = 1;    /* number of clients in master area */
 static const int resizehints = 0;    /* 1 means respect size hints in tiled resizals */
@@ -74,7 +95,7 @@ static const Layout layouts[] = {
 	{ NULL,       NULL },
 };
 
-/* key definitions */
+/* ----------- key definitions ----------- */
 #define MODKEY Mod4Mask
 #define TAGKEYS(KEY,TAG) \
 	{ MODKEY,                       KEY,      view,           {.ui = 1 << TAG} }, \
@@ -88,51 +109,66 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_cyan2, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_purple, NULL };
 static const char *termcmd[]  = { "st", NULL };
 static const char *shutcmd[]  = { "clpoweroff", NULL };
 static const char *rebcmd[]  = { "clreboot", NULL };
 
+/* ------------- keybindings ------------- */
 static Key keys[] = {
 	/* modifier               key              function        argument */
 	{ MODKEY|ShiftMask,   	  XK_Return,      spawn,          {.v = dmenucmd } },
 	{ MODKEY,                 XK_Return,      spawn,          {.v = termcmd } },
-	{ MODKEY,                 XK_b,           togglebar,      {0} },
-	{ MODKEY|ShiftMask,       XK_Right,       rotatestack,    {.i = +1 } },
-	{ MODKEY|ShiftMask,       XK_Left,        rotatestack,    {.i = -1 } },
+	{ MODKEY|ShiftMask, 	  	XK_r,      	   	quit,           {1} },
+
+	/* ---------- layouts ---------- */
+	{ MODKEY,       					XK_F9,          setlayout,      {.v = &layouts[0]} },
+	{ MODKEY,       					XK_F10,         setlayout,      {.v = &layouts[1]} },
+	{ MODKEY,       					XK_F11,         setlayout,      {.v = &layouts[2]} },
+
+	/* ----------- stack ----------- */
+	{ MODKEY,	          			XK_q,           killclient,     {0} },
 	{ MODKEY,                 XK_Right,       focusstack,     {.i = +1 } },
 	{ MODKEY,                 XK_Left,        focusstack,     {.i = -1 } },
 	{ MODKEY,                 XK_Down,        focusstack,     {.i = +1 } },
 	{ MODKEY,                 XK_Up,          focusstack,     {.i = -1 } },
-	{ MODKEY|ControlMask,  	  XK_minus,       setmfact,       {.f = -0.02} },
-	{ MODKEY|ControlMask,     XK_plus,        setmfact,       {.f = +0.02} },
-	{ MODKEY,       	  			XK_Tab,         focusstack,     {.i = +1 } },
-	{ MODKEY,                 XK_backslash,   view,           {0} },
-	{ MODKEY,	          			XK_q,           killclient,     {0} },
-	{ MODKEY,       					XK_F9,          setlayout,      {.v = &layouts[0]} },
-	{ MODKEY,       					XK_F10,         setlayout,      {.v = &layouts[1]} },
-	{ MODKEY,       					XK_F11,         setlayout,      {.v = &layouts[2]} },
-	{ MODKEY|ShiftMask,       XK_space,       togglefloating, {0} },
-	{ MODKEY,                 XK_0,           view,           {.ui = ~0 } },
-	{ MODKEY|ShiftMask,       XK_0,           tag,            {.ui = ~0 } },
-	{ MODKEY,                 XK_comma,       focusmon,       {.i = -1 } },
-	{ MODKEY,                 XK_period,      focusmon,       {.i = +1 } },
-	{ MODKEY|ShiftMask,       XK_comma,       tagmon,         {.i = -1 } },
-	{ MODKEY|ShiftMask,       XK_period,      tagmon,         {.i = +1 } },
-	{ MODKEY|ShiftMask, 	  	XK_r,      	   	quit,           {1} },
+	{ MODKEY|ShiftMask,       XK_Right,       rotatestack,    {.i = +1 } },
+	{ MODKEY|ShiftMask,       XK_Left,        rotatestack,    {.i = -1 } },
 	{ MODKEY,                 XK_i,      	   	incnmaster,     {.i = +1 } },
   { MODKEY,           			XK_d,      	   	incnmaster,     {.i = -1 } },
+	{ MODKEY,       	  			XK_Tab,         focusstack,     {.i = +1 } },
 	{ MODKEY,                 XK_z, 	   			zoom,           {0} },
+
+	/* ------------ gaps ----------- */
+	{ MODKEY,     						XK_g,      	   togglegaps,      {0} },
+	{ MODKEY|ShiftMask,     	XK_g,      	   defaultgaps,     {0} },
+
+	/* ---------- monitor ---------- */
+	{ MODKEY,                 XK_comma,      focusmon,        {.i = -1 } },
+	{ MODKEY,                 XK_period,     focusmon,        {.i = +1 } },
+	{ MODKEY|ShiftMask,       XK_comma,      tagmon,          {.i = -1 } },
+	{ MODKEY|ShiftMask,       XK_period,     tagmon,          {.i = +1 } },
+
+	/* ----------- resize ---------- */
+	{ MODKEY|ControlMask,  	  XK_minus,       setmfact,       {.f = -0.02} },
+	{ MODKEY|ControlMask,     XK_plus,        setmfact,       {.f = +0.02} },
+	{ MODKEY,                 XK_space,       togglefloating, {0} },
+
+	/* ------------ bar ------------ */
+	{ MODKEY,                 XK_b,           togglebar,      {0} },
+
+	/* ------------ tag ------------ */
+	{ MODKEY,                 XK_0,           view,           {.ui = ~0 } },
+	{ MODKEY|ShiftMask,       XK_0,           tag,            {.ui = ~0 } },
+	{ MODKEY,                 XK_backslash,   view,           {0} },
+
+	/* --------- scratchpad -------- */
 	{ MODKEY,            			XK_less,  	    togglescratch,  {.ui = 0 } },
 	{ MODKEY,            			XK_a,	          togglescratch,  {.ui = 1 } },
 
-    /* Gaps keybinds control */
-	{ MODKEY,     						XK_g,      	   togglegaps,     {0} },
-	{ MODKEY|ShiftMask,     				XK_g,      	   defaultgaps,     {0} },
-
     /* Apps Launched with SUPER + ALT + KEY  */
+	/* ----------- apps ----------- */
 	{ MODKEY,        	  XK_w,      	  spawn,          CMD ("xdotool search --class Chromium windowactivate || chromium") },
-/*  { MODKEY|ShiftMask, XK_space,     spawn,          CMD ("passmenu --type") },*/
 	{ MODKEY|ShiftMask, XK_t,      	  spawn,          CMD ("tnoty") },
 	{ MODKEY,        	  XK_j,      	  spawn,          CMD ("st -e journalctl --follow") },
 	{ MODKEY,        	  XK_s,      	  spawn,          CMD ("dmsearch") },
@@ -150,13 +186,18 @@ static Key keys[] = {
   { MODKEY,           XK_m, 	   		spawn,          SHCMD ("pamixer --toggle-mute") },
 	{ MODKEY,        	 	XK_1,      	  spawn,          CMD("xdotool search --class qutebrowser windowactivate || qutebrowser") },
 	{ MODKEY,        	  XK_2,      	  spawn,          CMD("xdotool search --class st-256color windowactivate || st") },
+
+	/* ------------ tag ------------ */
 	TAGKEYS(            XK_1,            0)
 	TAGKEYS(            XK_2,            1)
 	TAGKEYS(            XK_3,            2)
 	TAGKEYS(            XK_4,            3)
+	TAGKEYS(            XK_5,            4)
+	TAGKEYS(            XK_6,            5)
+	TAGKEYS(            XK_7,            6)
 };
 
-/* button definitions */
+/* ---------- button definitions --------- */
 /* click can be ClkTagBar, ClkLtSymbol, ClkStatusText, ClkWinTitle, ClkClientWin, or ClkRootWin */
 static Button buttons[] = {
 	/* click                event mask      button          function        argument */
