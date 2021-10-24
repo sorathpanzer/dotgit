7dea09c205bd81257953ef167a481d2a99bd5689
sorathpanzer <sorathpanzer@gmail.com>
2021-10-24 01:48:20 +0100

    some updates

--- dotfiles/.config/suckless/autostart.sh
+++ dotfiles/.config/suckless/autostart.sh
@@ -1,21 +1,24 @@
 #!/bin/sh
 
-slstatus &
+dwmblocks &
 udiskie &
 numlockx &
 xrdb -load $HOME/.Xresources &
 xrandr --output "eDP-1" --brightness 0.70 &
-#xmodmap -e 'keycode 50 = Escape' &
 xinput --set-prop "SYNA2B46:00 06CB:CD5F Touchpad" "libinput Natural Scrolling Enabled" 1 &
 pactl set-sink-volume alsa_output.pci-0000_00_1f.3.analog-stereo 100% &
-#/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
 picom --config ~/.config/picom/picom.conf -b &
 syncthing -no-browser &
-hsetroot -cover $HOME/Imagens/Wallpapers/$(ls $HOME/Imagens/Wallpapers | shuf -n 1) &
-sudo wg-quick up wg0 &
-#hsetroot -solid "#000000" &
+hsetroot -cover $HOME/Imagens/Wallpapers/mars.jpg &
+#sudo wg-quick up wg0 &
+#hsetroot -cover $HOME/Imagens/Wallpapers/$(ls $HOME/Imagens/Wallpapers | shuf -n 1) &
 
 if uname -n | grep -q 'void-live'; then
   pulseaudio --daemonize=no --exit-idle-time=-1
 fi
 
+#--------SpeedUp keyboard
+xset r rate 190 40 &
+#--------Don't let computer sleep
+xset s 0 0 &
+xset s off &
