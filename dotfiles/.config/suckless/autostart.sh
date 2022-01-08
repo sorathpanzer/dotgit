86e71edc683ba7e8461916890ff4e5f1ad08f28f
sorathpanzer <sorathpanzer@gmail.com>
2022-01-08 19:36:08 +0000

    reduce brightness

--- dotfiles/.config/suckless/autostart.sh
+++ dotfiles/.config/suckless/autostart.sh
@@ -6,7 +6,7 @@ dwmblocks &
 udiskie &
 numlockx &
 xrdb -load $HOME/.Xresources &
-xrandr --output "eDP-1" --brightness 0.70 &
+xrandr --output "eDP-1" --brightness 0.60 &
 xinput --set-prop "SYNA2B46:00 06CB:CD5F Touchpad" "libinput Natural Scrolling Enabled" 1 &
 pactl set-sink-volume alsa_output.pci-0000_00_1f.3.analog-stereo 100% &
 picom --config ~/.config/picom/picom.conf -b &
